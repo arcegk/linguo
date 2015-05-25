57d187aac3715f61f08a220a8d100e946e1cd714
Kevin <arcegk@gmail.com>
2015-05-25 12:11:44 -0500

    #8

--- web/views.py
+++ web/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-from .models import Profesor
+from .models import Profesor , Contenido
 # Create your views here.
 
 class HomeTemplateView(TemplateView):
 	template_name = "index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(HomeTemplateView, self).get_context_data(**kwargs)
+		context['features'] = Contenido.objects.filter(seccion="INICIO")
+		return context
 
 
 class ProfesorListView(ListView):
