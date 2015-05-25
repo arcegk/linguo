715e00fa6d1a0e8a36d470b87aa4156ea518eef8
Kevin <arcegk@gmail.com>
2015-05-25 11:21:03 -0500

    #7.5

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Idioma , Profesor
+from .models import Idioma , Profesor , Contenido
 
 # Register your models here.
 admin.site.register(Idioma)
