099e59fb8b7c1a548ad8fae1ebfd3e73b6d5f040
Kevin <arcegk@gmail.com>
2016-03-22 12:09:36 -0500

    gg ez

--- linguoweb/settings.py
+++ linguoweb/settings.py
@@ -25,7 +25,7 @@ env = environ.Env()
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
